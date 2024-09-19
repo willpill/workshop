6d9a3212bb5317b93a51aa6bd8d40f60dd630071
Yinwei Zhang <yzhang50@stuy.edu>
2024-09-19 11:42:03 -0400

    Added read file logic

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,2 +1,23 @@
-import random
+# Yinwei Zhang
+# China Rats Plus One
+# SoftDev
+# <K06: CSV file parsing>
+# 2024-9-19
+# Time Spent : 0.5 Hours
 
+import random
+import csv
+
+def readfile(f):
+    dictionary = {}
+    with open (f, 'r') as listfile:
+        reader = csv.reader(listfile)
+        next(reader)
+        for row in reader:
+            job = row[0]
+            percent = float(row[1])
+            dictionary[job] = percent
+            
+    print(dictionary)
+        
+readfile("occupations.csv")
