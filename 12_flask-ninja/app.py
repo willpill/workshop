9dba023a9a9f6634f6ac364efe492026eb087c49
Yinwei Zhang <yzhang50@stuy.edu>
2024-10-08 10:11:31 -0400

    Add work14-15 and missing files

---
+++ 12_flask-ninja/app.py
@@ -0,0 +1,50 @@
+# Yinwei Zhang
+# SoftDev
+# Sep 2024
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Q0:
+navigating to /my_foist_template directory will result in an error BUT not error if we go to the root path /
+
+Q1:
+No, but it is: https://127.0.0.1:5000/my_foist_template
+
+Q2:
+It tells us which file to use as template, and the parameters for the variables used
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Q Where is my_foist_template and where is render_template?
+NOTE It seems that we would not necessarily need to import render_template. We thought it would give us an error when removed, but that is not the case.
+When you use render template, the URL is not the path to the html in your directory but the route you set in the decorator.
+    
+"""
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Q0: What will happen if you remove render_template from the following statement?
+# (log prediction before executing...)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+coll = [0,1,1,2,3,5,8]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Q1: Can all of your teammates confidently predict the URL to use to load this page?
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@app.route("/my_foist_template") 
+def test_tmplt():
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Q2: What is the significance of each argument? Simplest, most concise answer best.
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
