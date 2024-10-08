035b2fc39e6ea890df3c6e5824329bf54c36826f
Yinwei Zhang <yzhang50@stuy.edu>
2024-10-08 19:23:02 -0400

    Finish 15

--- 15_flask-forms/app.py
+++ 15_flask-forms/app.py
@@ -2,10 +2,21 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/greet', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('login.html')
 
+@app.route('/greet', methods=['GET', 'POST'])
 def greetings_page():
-    print("")
+    if request.method == 'POST':
+        username = request.form['username']
+        requesttype = "POST"
+    else:
+        username = request.args.get('username')
+        requesttype = "GET"
+
+    return render_template('response.html', username=username, request=requesttype)
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
